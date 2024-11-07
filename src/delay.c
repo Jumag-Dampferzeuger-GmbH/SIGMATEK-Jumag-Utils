ce5d7c22bd227f8ac16398306755676cee612efb
Alexander Tonn <alexandertonn@gmx.de>
2024-11-07 09:00:21 +0100

    re added the simple delay functions if the user only needs a delay timer without info about the elapsed time

--- src/delay.c
+++ src/delay.c
@@ -1,3 +1,66 @@
+/* 
+
+Function Global __cdecl DelayTon
+VAR_INPUT
+  IN : BOOL;
+  PT : DINT; 
+  ET : ^UDINT; 
+  CLOCK : UDINT;
+END_VAR
+VAR_OUTPUT
+  Q : BOOL;
+END_VAR;
+
+*/
+// turn on the output Q after the input IN has been on for the time PT. 
+// The time is measured in CLOCK units. The elapsed time is stored in ET.
+extern "C" bool DelayTon (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
+{
+    if (!xCondition) 
+    {
+        *uiElapsedTime = uiClock; 
+        return false;
+    }
+    else
+    {
+        if (uiClock > (*uiElapsedTime + iTargetTime)) 
+            return true;
+        else
+            return false;
+    }
+}
+
+/* 
+
+Function Global __cdecl DelayTof
+VAR_INPUT
+  IN : BOOL;
+  PT : DINT; 
+  ET : ^UDINT; 
+  CLOCK : UDINT;
+END_VAR
+VAR_OUTPUT
+  Q : BOOL;
+END_VAR;
+
+*/
+// turn on the output for a certain time if IN Condition is true
+extern "C" bool DelayTof (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
+{
+    if (xCondition) 
+    {
+        *uiElapsedTime = uiClock; 
+        return true; 
+    }
+    else
+    {    
+        if (uiClock > (*uiElapsedTime + iTargetTime)) 
+            return false;
+        else
+            return true; 
+    }
+}
+
 /*
 
 Function Global __cdecl DelayTon
@@ -13,9 +76,10 @@ VAR_OUTPUT
 END_VAR;
 
 */
+// ADVANCED TON FUNCTION WICH RETURNS THE ELAPSED TIME
 // turn on the output Q after the input IN has been on for the time PT.
 // The time is measured in CLOCK units. The elapsed time is stored in ET.
-extern "C" bool DelayTon(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
+extern "C" bool DelayTonE(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
   if (!condition)
   {
@@ -53,8 +117,9 @@ VAR_OUTPUT
 END_VAR;
 
 */
+// ADVANCED TON FUNCTION WICH RETURNS THE ELAPSED TIME
 // turn on the output for a certain time if IN Condition is true
-extern "C" bool DelayTof(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
+extern "C" bool DelayTofE(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
   if (condition)
   {
