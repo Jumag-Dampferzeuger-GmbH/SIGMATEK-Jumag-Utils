bd86fe823d810a8d0053d4ae72a1e305864a5b8e
Alexander Tonn <alexandertonn@gmx.de>
2025-01-20 10:57:01 +0100

    added new timeConversion.c | modified math.c with a new DINT min function, all argument lists of the functions optimized

--- src/math.c
+++ src/math.c
@@ -14,16 +14,16 @@ END_VAR;
 
 
 // calc the min value of two float values
-extern "C" float fMin(float f1, float f2)
+extern "C" float fMin(const float f1, const float f2)
 {
     return (f1 < f2) ? f1 : f2;
 }
 
 /* 
 
-Function Global __cdecl aMin
+Function Global __cdecl aMinF
 VAR_INPUT
-  INPUT : ^REAL;
+  pINPUT : ^REAL;
   SIZE : DINT; 
 END_VAR
 VAR_OUTPUT
@@ -33,7 +33,7 @@ END_VAR;
 */
 
 // calc the min value of an array of float values
-extern "C" float aMin(float *f , int size)
+extern "C" float aMinF(const float *f , const long int size)
 {
     float fMin = f[0];
 
@@ -60,16 +60,16 @@ END_VAR;
 
 
 // calc the max value of two float values
-extern "C" float fMax(float f1, float f2)
+extern "C" float fMax(const float f1, const float f2)
 {
   return (f1 > f2) ? f1 : f2;
 }
 
 /* 
 
-Function Global __cdecl aMax
+Function Global __cdecl aMaxF
 VAR_INPUT
-  INPUT : ^REAL;
+  pINPUT : ^REAL;
   SIZE : DINT; 
 END_VAR
 VAR_OUTPUT
@@ -79,7 +79,7 @@ END_VAR;
 */
 
 // calc the max value of an array of float values
-extern "C" float aMax(float *f , int size)
+extern "C" float aMaxF(const float *f , const int size)
 {
     float fMax = f[0];
 
@@ -93,6 +93,34 @@ extern "C" float aMax(float *f , int size)
 
 /* 
 
+Function Global __cdecl aMinF
+VAR_INPUT
+  pINPUT : ^DINT;
+  SIZE : DINT; 
+END_VAR
+VAR_OUTPUT
+  Q : DINT;
+END_VAR;
+
+*/
+
+// calc the min value of an array of DINTs
+extern "C" long int aMin(const long int *input , const long int size)
+{
+    long int minValue = input[0];
+
+    for (int i = 1; i < size; i++)
+    {
+        if (input[i] < minValue)
+            minValue = input[i];
+    }
+    return minValue; 
+}
+
+
+
+/* 
+
 Function Global __cdecl isFinite
 VAR_INPUT
   INPUT : REAL; 
@@ -103,14 +131,14 @@ END_VAR;
 
 */ 
 // true (1) if f is finite.
-extern "C" bool isFinite(float f)
+extern "C" bool isFinite(const float f)
 {
     return (f <= FLOAT_MAXVAL && f >= -FLOAT_MAXVAL) ? true : false;
 }
 
 /* 
 
-Function Global __cdecl isGreaterF
+Function Global __cdecl isGreater
 VAR_INPUT
   CMP1 : REAL;
   CMP2 : REAL; 
@@ -121,14 +149,14 @@ END_VAR;
 
 */
 // true (1) if f1 is greater than f2.
-extern "C" bool isGreaterF(float f1, float f2)
+extern "C" bool isGreater(const float f1, const float f2)
 {
     return (f1 > f2) ? true : false;
 }
 
 /* 
 
-Function Global __cdecl isGreaterEqualF
+Function Global __cdecl isGreaterEqual
 VAR_INPUT
   CMP1 : REAL;
   CMP2 : REAL; 
@@ -139,77 +167,15 @@ END_VAR;
 
 */
 // true (1) if f1 is greater equal than f2.
-extern "C" bool isGreaterEqualF(float f1, float f2)
+extern "C" bool isGreaterEqual(const float f1, const float f2)
 {
     return (f1 >= f2) ? true : false;
 }
 
-/* 
 
-Function Global __cdecl isEqualF
-VAR_INPUT
-  CMP1 : REAL;
-  CMP2 : REAL; 
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
 
-*/
-// true (1) if f1 is equal to f2.
-extern "C" bool isEqualF(float f1, float f2)
-{
-    return (f1 == f2) ? true : false;
-}
-/* 
 
-Function Global __cdecl isEqualI
-VAR_INPUT
-  CMP1 : DINT;
-  CMP2 : DINT; 
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
 
-*/
-extern "C" bool isEqualI(int i1, int i2)
-{
-    return (i1 == i2) ? true : false;
-}
 
-/* 
 
-Function Global __cdecl isUnequalI
-VAR_INPUT
-  CMP1 : DINT;
-  CMP2 : DINT; 
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
 
-*/
-// returns true
-extern "C" bool isUnequalI(int i1, int i2)
-{
-    return (i1 != i2) ? true : false;
-}
-
-/* 
-
-Function Global __cdecl isUnequalF
-VAR_INPUT
-  CMP1 : REAL;
-  CMP2 : REAL; 
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
-
-*/
-// returns true
-extern "C" bool isUnequalF(float f1, float f2)
-{
-    return (f1 != f2) ? true : false;
-}
