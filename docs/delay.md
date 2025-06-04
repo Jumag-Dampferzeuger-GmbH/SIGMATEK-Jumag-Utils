d0c0cca92a709bc5fb2fa06bb7e4a324d171e67a
Alexander Tonn <alexandertonn@gmx.de>
2025-06-04 09:16:08 +0200

    added tonOff in delay.c

--- docs/delay.md
+++ docs/delay.md
@@ -8,6 +8,9 @@ The ton and tof functions are used for delaying signals.
     - [Lasal Class 2 Example](#lasal-class-2-example)
     - [ST Code](#st-code)
     - [Class View](#class-view)
+    - [DelayTonOff](#delaytonoff)
+    - [Lasal Class 2 Example](#lasal-class-2-example-1)
+    - [ST Code](#st-code-1)
 
 ## Syntax in Lasal Class 2
 
@@ -95,3 +98,63 @@ END_FUNCTION
 
 ### Class View
 ![alt text](img/image-12.png)
+
+
+### DelayTonOff
+* if the input signal is switching from false to true the output is true after reaching the PT_on time. If the input signal is switching to false the output will be false after the PT_off time.
+
+
+```
+Q : BOOL = DelayTon(IN : BOOL, PT_ON : DINT , PT_OFF : DINT ,ET_ON: ^UDINT ,ET_OFF: ^UDINT , TIMESTAMP: ^UDINT, CLOCK : UDINT)
+```
+
+| Argument | Datatype | Meaning |
+| ------------- | ------------- | ------------- |
+| IN  | BOOL | Input signal |
+| PT_ON  | DINT | On Delay time in ms |
+| PT_OFF  | DINT | Off Delay time in ms |
+| ET_ON  | ^UDINT | Pointer to the elapsed On time |
+| ET_OFF  | ^UDINT | Pointer to the elapsed Off time |
+| TIMESTAMP  | ^UDINT | Pointer to the timestamp for saving the  ```ops.tAbsolute```  time|
+| CLOCK  | UDINT | Clock signal ```ops.tAbsolute``` |
+| Q  | BOOL | Output signal |
+
+
+
+### Lasal Class 2 Example
+* You can find a compialable example by clicking here: [Example](https://github.com/Jumag-Dampferzeuger-GmbH/SIGMATEK-Jumag-Utils-Examples/tree/main/delay)
+
+### ST Code
+
+```
+
+Function Global __cdecl DelayTonOff
+VAR_INPUT
+  IN : BOOL;
+  PT_ON : DINT;
+  PT_OFF : DINT;
+  ET_ON : ^UDINT;
+  ET_OFF : ^UDINT;
+  TIMESTAMP : ^UDINT;
+  CLOCK : UDINT;
+END_VAR
+VAR_OUTPUT
+  Q : BOOL;
+END_VAR;
+
+FUNCTION VIRTUAL GLOBAL tonof::CyWork
+	VAR_INPUT
+		EAX 	: UDINT;
+	END_VAR
+	VAR_OUTPUT
+		state (EAX) 	: UDINT;
+	END_VAR
+  
+  ClassSvr := DelayTonOff(IN:=input=1, PT_ON:=targetTimeOn, PT_OFF:=targetTimeOff, ET_ON:=#elapsedOn, ET_OFF:=#elapsedOff, TIMESTAMP:=#ts, CLOCK:=ops.tAbsolute);
+
+	state := READY;
+
+END_FUNCTION
+
+
+```
--- src/delay.c
+++ src/delay.c
@@ -1,66 +1,3 @@
-/* 
-
-Function Global __cdecl DelayTon
-VAR_INPUT
-  IN : BOOL;
-  PT : DINT; 
-  ET : ^UDINT; 
-  CLOCK : UDINT;
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
-
-*/
-// turn on the output Q after the input IN has been on for the time PT. 
-// The time is measured in CLOCK units. The elapsed time is stored in ET.
-extern "C" bool DelayTon (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
-{
-    if (!xCondition) 
-    {
-        *uiElapsedTime = uiClock; 
-        return false;
-    }
-    else
-    {
-        if (uiClock > (*uiElapsedTime + iTargetTime)) 
-            return true;
-        else
-            return false;
-    }
-}
-
-/* 
-
-Function Global __cdecl DelayTof
-VAR_INPUT
-  IN : BOOL;
-  PT : DINT; 
-  ET : ^UDINT; 
-  CLOCK : UDINT;
-END_VAR
-VAR_OUTPUT
-  Q : BOOL;
-END_VAR;
-
-*/
-// turn on the output for a certain time if IN Condition is true
-extern "C" bool DelayTof (bool xCondition, int iTargetTime, unsigned int *uiElapsedTime, unsigned int uiClock)
-{
-    if (xCondition) 
-    {
-        *uiElapsedTime = uiClock; 
-        return true; 
-    }
-    else
-    {    
-        if (uiClock > (*uiElapsedTime + iTargetTime)) 
-            return false;
-        else
-            return true; 
-    }
-}
-
 /*
 
 Function Global __cdecl DelayTon
@@ -76,10 +13,9 @@ VAR_OUTPUT
 END_VAR;
 
 */
-// ADVANCED TON FUNCTION WICH RETURNS THE ELAPSED TIME
 // turn on the output Q after the input IN has been on for the time PT.
 // The time is measured in CLOCK units. The elapsed time is stored in ET.
-extern "C" bool DelayTonE(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
+extern "C" bool DelayTon(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
   if (!condition)
   {
@@ -117,9 +53,8 @@ VAR_OUTPUT
 END_VAR;
 
 */
-// ADVANCED TON FUNCTION WICH RETURNS THE ELAPSED TIME
 // turn on the output for a certain time if IN Condition is true
-extern "C" bool DelayTofE(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
+extern "C" bool DelayTof(bool condition, int targetTime, unsigned int *elapsed, unsigned int *timestamp, unsigned int clock)
 {
   if (condition)
   {
@@ -141,4 +76,81 @@ extern "C" bool DelayTofE(bool condition, int targetTime, unsigned int *elapsed,
       return true;
     }
   }
+}
+/*
+
+Function Global __cdecl DelayTonOff
+VAR_INPUT
+  IN : BOOL;
+  PT_ON : DINT;
+  PT_OFF : DINT;
+  ET_ON : ^UDINT;
+  ET_OFF : ^UDINT;
+  TIMESTAMP : ^UDINT;
+  CLOCK : UDINT;
+END_VAR
+VAR_OUTPUT
+  Q : BOOL;
+END_VAR;
+
+*/
+// turn on the output for a certain time if IN Condition is true
+extern "C" bool DelayTonOff(const bool condition, const int targetOn, const int targetOff, unsigned int *elapsedOn,unsigned int *elapsedOff, unsigned int *timestamp, const unsigned int clock)
+{
+
+  if( (!condition && *elapsedOn >= targetOn && *elapsedOff >= targetOff) ||
+      (!condition && *elapsedOn == 0 && *elapsedOff == 0) ||
+      (!condition && *elapsedOn > 0 && *elapsedOn < targetOn) )
+  {
+    // reset timers
+    *elapsedOn = 0; 
+    *elapsedOff = 0; 
+    *timestamp = clock;
+    return false; 
+  }
+
+
+  //on timer
+  if(condition && *elapsedOn < targetOn)
+  {
+    if(clock > (*timestamp + targetOn))
+    {
+      *elapsedOn = targetOn;
+      return true; 
+    }
+    else 
+    {
+      *elapsedOn = clock - *timestamp;
+      return false; 
+    }
+  }
+
+  if(condition && *elapsedOn >= targetOn)
+  {
+    *timestamp = clock;
+    return true; 
+  }
+
+  if (!condition && *elapsedOn >= targetOn)
+  {
+    // off timer
+    if(clock > (*timestamp + targetOff))
+    {
+      *elapsedOff = targetOff;
+      return false; 
+    }
+    else 
+    {
+      *elapsedOff = clock - *timestamp;
+      return true; 
+    }
+  }
+
+  if(!condition && *elapsedOff >= targetOff)
+  {
+    *timestamp = clock;
+    return false; 
+  }
+
+
 }
